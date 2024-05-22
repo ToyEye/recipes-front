ff5368ad50e8ee23bcde8ee4bed60489a02c9724
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-05-22 12:57:57 +0300

    change server

--- app/apiServise/apiServise.tsx
+++ app/apiServise/apiServise.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001/api";
+axios.defaults.baseURL = "https://recipes-server-83pi.onrender.com/api";
 
 export const getRecipes = async () => {
   const { data } = await axios.get("/recipes");
